1b4e1ffd05c2de3f9d20baaa5b3bbc234a06117f
azizjan18 <websbyaziz@gmail.com>
2025-04-11 23:19:11 +0500

    utils code discussion

---
+++ src/utils/apierrorhandler.js
@@ -0,0 +1,27 @@
+class API_ERROR extends ERROR {
+    constructor(statusCode, 
+        message = "something went wrong",
+        error = [],
+        statk =""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = false
+        this.error = error
+
+
+
+
+        if (statck) {
+            this.statck = statk
+        }
+        else{
+            ERROR.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+
+export { API_ERROR};
